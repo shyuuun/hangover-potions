196aa9733e686029e4fa4edbbf0d59327f5f8393
shyuuun <m4rshm4ll0w14@gmail.com>
2022-11-02 04:06:08 +0800

    added csrf trusted origin

--- cilent/settings.py
+++ cilent/settings.py
@@ -126,3 +126,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://hangoverpotions.azurewebsites.net'
+]
